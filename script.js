4b08e9601780feb175196fa95848b7748dfac589
Abhilash4597 <abhilash4597@gmail.com>
2023-02-10 12:34:54 +0530

    functionality added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const modalBtns = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelectorAll('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+modalBtns.forEach((ele) => {
+  ele.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  })
+})
+
+closeModalBtn.forEach(ele => {
+  ele.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+});
