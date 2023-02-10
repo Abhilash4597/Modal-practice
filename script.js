0aab0076e1cea61aa8493590d7e49a8654afe6ad
Abhilash4597 <abhilash4597@gmail.com>
2023-02-10 16:59:12 +0530

    keydown event added and code refactoring done

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 'use strict';
 
 const modalBtns = document.querySelectorAll('.show-modal');
-const closeModalBtn = document.querySelectorAll('.close-modal');
+const closeModalBtn = document.querySelector('.close-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
-modalBtns.forEach((ele) => {
-  ele.addEventListener('click', () => {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  })
-})
+function removeHidden() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
 
-closeModalBtn.forEach(ele => {
-  ele.addEventListener('click', () => {
+function addHidden() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+modalBtns.forEach(ele => {
+  ele.addEventListener('click', removeHidden);
+});
+
+function keyPreseed(e) {
+  let value = e.key;
+  if (value === 'Escape') {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-  });
-});
+  }
+}
+
+closeModalBtn.addEventListener('click', addHidden);
+overlay.addEventListener('click', addHidden);
+document.addEventListener('keydown', keyPreseed);
